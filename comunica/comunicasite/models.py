f0bc513dddb1ab1f5684ac6f9660873db8d1e4a3
Duda Meneses <me157282@gmail.com>
2023-06-18 05:22:10 -0300

    230618

--- comunica/comunicasite/models.py
+++ comunica/comunicasite/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Comentario(models.Model):
+    nome = models.CharField(max_length=20)
+    comentario = models.TextField()
+    publicado = models.DateTimeField(auto_now_add=True)
+    ativo = models.BooleanField(default=False)
+
+class Meta:
+    ordering = ['publicado']
+
+def __str__(self):
+    return 'Comment {} by {}'.format(self.body, self.nome)
+
+

4f71b841f858d5741b3476e441e85230688e9b62
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2023-06-28 18:24:23 -0300

    arrumar campos do formulário e estilizar

--- comunica/comunicasite/forms.py
+++ comunica/comunicasite/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 from .models import Comentario
 
-class ComentarioForm(forms.ModelForm):
+
+class ComentarioForm(forms.Form):
     class Meta:
         model = Comentario
         fields = ['nome', 'comentario']
-
-    def save(self, commit=True):
-        comentario = super().save(commit=False)
-        comentario.status = 'pendente'  # Define o status como pendente
-        if commit:
-            comentario.save()
-        return comentario
-
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+    nome = forms.CharField(label="Nome", max_length=50, required=True,
+                           widget=forms.TextInput(attrs={'class': 'form-control'}))
+    comentario = forms.CharField(
+        label="Comentário", max_length=200, required=True, widget=forms.Textarea(attrs={'class': 'form-control'}))
--- comunica/comunicasite/views.py
+++ comunica/comunicasite/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .forms import ComentarioForm
 from .models import Comentario
+from .forms import ComentarioForm
 from django.contrib import messages
 
 # Create your views here.
-def index(request):
-    return render(request, 'site/index.html')
 
-def comentarios(request):
+
+def index(request):
     if request.method == 'POST':
         form = ComentarioForm(request.POST)
         if form.is_valid():
-            form.save()
+            # form.salvar()
             return redirect('comentarios')
     else:
         form = ComentarioForm()
-    
+
     comentarios = Comentario.objects.all()
-    
+
     context = {
         'form': form,
         'comentarios': comentarios
     }
-    
-    return render(request, 'site/comentarios.html', context)
+
+    return render(request, './site/index.html', context)
+
 
 def comentarios(request):
     comentarios_aprovados = Comentario.objects.filter(aprovado=True)
@@ -32,20 +32,18 @@ def comentarios(request):
     if request.method == 'POST':
         form = ComentarioForm(request.POST)
         if form.is_valid():
-            comentario = form.save(commit=False)
-            comentario.status = 'pendente'  # Define o status como pendente
-            comentario.save()
-        
-            messages.success(request, 'Comentário enviado com sucesso! Aguardando aprovação.')
+            comentario = Comentario
 
-            return redirect('comentarios')
-        
+            # messages.success(request, 'Comentário enviado com sucesso! Aguardando aprovação.')
+
+            # return redirect('comentarios')
+
     else:
         form = ComentarioForm()
-    
+
     context = {
         'form': form,
         'comentarios': comentarios_aprovados
     }
-    
+
     return render(request, 'site/index.html', context)
